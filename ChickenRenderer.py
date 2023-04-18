02ceb58c63e4a59105a2531bed29f3a2cbd7665e
Chooken <joshuawereszczuk@gmail.com>
2023-04-18 13:00:52 +1000

    New Framework Finished

--- ChickenRenderer.py
+++ ChickenRenderer.py
@@ -1,31 +1,74 @@
-import tkinter 
-from tkinter import ttk
+from PyQt6.QtWidgets import QLabel, QWidget, QGraphicsView, QGraphicsScene
+from PyQt6.QtGui import QPixmap, QPainter
+from PyQt6.QtCore import Qt, QRectF
 import math
-from PIL import ImageTk, Image
-import copy
 
-class ChickenRenderer():
+class ChickenRenderer(QGraphicsView):
 
-    def __init__(self, sprite_window):
+    def __init__(self):
 
-        ## DEBUG VISUALS
-        self.body = Image.open("test_ercle.png").resize((250,250),Image.LANCZOS) 
+        ## Graphics Setup
+        self.canvas = QGraphicsScene()
+        super(QGraphicsView, self).__init__(self.canvas, None)
 
-        self.face = Image.open("test_face.png").resize((250,250),Image.LANCZOS)
+        ## Window Setup
+        self.setWindowTitle("Stupid 2 - AI Companion")
+        self.resize(500,500)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setStyleSheet("background: transparent")
+        self.setRenderHints(QPainter.RenderHint.Antialiasing | QPainter.RenderHint.SmoothPixmapTransform)
+    
+        ## Loading and Placing Images
+        self.body_pixmap = QPixmap("test_ercle")
+        self.body = self.canvas.addPixmap(self.body_pixmap.scaled(250, 250, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+        self.body.setPos(10, self.height() - self.body.boundingRect().height() - 10)
 
-        self.sprite = ImageTk.PhotoImage(self.body)  
+        self.face_pixmap = QPixmap("test_face")
+        self.face = self.canvas.addPixmap(self.face_pixmap.scaled(250, 250, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+        self.face.setPos(10, self.height() - self.face.boundingRect().height() - 10)
 
-        self.FaceImage = ImageTk.PhotoImage(self.face)
-
-        self.canvas = tkinter.Canvas(sprite_window, width=250, height=250, bg="black", bd=0, highlightthickness=0, relief='ridge')
-
-        self.body = self.canvas.create_image(125, 125, image=self.sprite)
-        self.face = self.canvas.create_image(125, 125, image=self.FaceImage)
-
-        self.canvas.pack(anchor = "w", side = "bottom")
+        ## Setting SceneRect to fixed position
+        rect = QRectF(self.rect())
+        self.fitInView(rect)
+        self.setSceneRect(rect)
 
     def Update(self, ms_since_startup):
 
-        ## DEBUG VISUALS
-        self.canvas.move(self.face, 0, math.sin((ms_since_startup - 500) * 0.002) * 0.15)
-        self.canvas.move(self.body, 0, math.sin(ms_since_startup * 0.002) * 0.2)
+        ## DEBUG VISUALS - Animations
+        self.face.setTransformOriginPoint(125,125)
+        self.face.moveBy(0, self.SinPos(ms_since_startup, 2, 0.5, 0.2))
+        self.body.moveBy(0, self.SinPos(ms_since_startup, 2, 0, 0.2))
+        self.face.setRotation(self.SinPos(ms_since_startup, 4, 0, 45))
+
+    def SinPos(self, ms_since_startup, period_in_seconds, offset_in_seconds, magnitude):
+        return math.sin(((ms_since_startup - (offset_in_seconds * 1000)) * 0.001) / period_in_seconds * 2) * magnitude
+
+    def SetBorderless(self, state):
+
+        if (state):
+            self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.WindowTransparentForInput)
+            self.showMaximized()
+
+            self.body.setPos(10, self.height() - self.body.boundingRect().height() - 10)
+            self.face.setPos(10, self.height() - self.face.boundingRect().height() - 10)
+
+            ## Setting SceneRect to fixed position
+            rect = QRectF(self.rect())
+            self.fitInView(rect)
+            self.setSceneRect(rect)
+
+            self.show()
+        else:
+            self.resize(500,500)
+            self.setWindowFlag(Qt.WindowType.FramelessWindowHint, False)
+            self.setWindowFlag(Qt.WindowType.WindowTransparentForInput, False)
+
+            self.body.setPos(10, self.height() - self.body.boundingRect().height() - 10)
+            self.face.setPos(10, self.height() - self.face.boundingRect().height() - 10)
+
+            ## Setting SceneRect to fixed position
+            rect = QRectF(self.rect())
+            self.fitInView(rect)
+            self.setSceneRect(rect)
+
+            self.show()

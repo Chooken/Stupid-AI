0113f28eb2539156d77804c39daafdec4f24c3aa
Chooken <joshuawereszczuk@gmail.com>
2023-04-20 10:00:33 +1000

    Making My Life easier

--- ChickenRenderer.py
+++ ChickenRenderer.py
@@ -24,30 +24,29 @@ class ChickenRenderer(QGraphicsView):
 
         ## Loading and Placing Images
         self.body_pixmap = self.LoadPixmap("test_ercle", 250, 250)
-        self.body = self.canvas.addPixmap(self.body_pixmap)
-
         self.face_pixmap = self.LoadPixmap("test_face", 250, 250)
-        self.face = self.canvas.addPixmap(self.face_pixmap)
-        self.face.setTransformOriginPoint(125,125)
+
+        self.parts = {
+            "body": self.canvas.addPixmap(self.body_pixmap),
+            "face": self.canvas.addPixmap(self.face_pixmap)
+        }
+
+        self.parts["face"].setTransformOriginPoint(125,125)
 
     def resizeEvent(self, event: QResizeEvent) -> None:
 
         super().resizeEvent(event)
 
-        face_offset = 0
-        body_offset = 0
+        for part in self.parts.values():
 
-        ## Calculate Original Offset
-        if (event.oldSize().height() == -1):
-            face_offset = self.face.boundingRect().height() + 10
-            body_offset = self.body.boundingRect().height() + 10
-        else:
-            face_offset = event.oldSize().height() - self.face.pos().y()
-            body_offset = event.oldSize().height() - self.body.pos().y()
+            ## Calculate Original Offset
+            if (event.oldSize().height() == -1):
+                offset = part.boundingRect().height() + 10
+            else:
+                offset = event.oldSize().height() - part.pos().y()
 
-        ## Sets New Position
-        self.body.setPos(10, self.height() - body_offset)
-        self.face.setPos(10, self.height() - face_offset)
+            ## Sets New Position
+            part.setPos(10, self.height() - offset)
 
         ## Setting SceneRect to fixed position
         rect = QRectF(self.rect())
@@ -66,15 +65,17 @@ class ChickenRenderer(QGraphicsView):
 
         ## DEBUG VISUALS - Animations
         
-        self.face.moveBy(0, self.SinPos(ms_since_startup, 2, 0.5, 0.2))
-        self.body.moveBy(0, self.SinPos(ms_since_startup, 2, 0, 0.2))
-        self.face.setRotation(self.SinPos(ms_since_startup, 4, 0, 45))
+        self.parts["face"].moveBy(0, self.SinPos(ms_since_startup, 2, 0.5, 0.2))
+        self.parts["body"].moveBy(0, self.SinPos(ms_since_startup, 2, 0, 0.2))
+
+        self.parts["face"].setRotation(self.SinPos(ms_since_startup, 4, 0, 45))
 
     def SinPos(self, ms_since_startup: int, period_in_seconds: float, offset_in_seconds: float, magnitude: float) -> float:
         return math.sin(((ms_since_startup - (offset_in_seconds * 1000)) * 0.001) / period_in_seconds * 2) * magnitude
 
     def setState(self, index: int):
 
+        ## Check for Index of state
         if (index == 0):
             self.resize(500,500)
             self.setWindowFlag(Qt.WindowType.FramelessWindowHint, False)
